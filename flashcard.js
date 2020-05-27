9275b8e439f512c3d31fa27355874b29ac1c8e87
Tracy Yeast-Rowser <tyeast-rowser@humana.com>
2020-05-27 11:10:52 -0400

    start of js, figured out animation css

--- flashcard.js
+++ flashcard.js
@@ -1,1 +1,7 @@
+//will be adding images later
+//will work on scoring/buttons/etc after I get flipping to work
+const cards = document.querySelectorAll('.card');
+cards.addEventListener( 'click', function() {
+  cards.classList.toggle('is-flipped');
+});
 
