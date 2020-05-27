640055908963b60e9e0d3adca8b902a95044949e
Tracy Yeast-Rowser <tyeast-rowser@humana.com>
2020-05-27 18:35:17 -0400

    started on arrays, functions, updated css

--- flashcard.js
+++ flashcard.js
@@ -1,41 +1,35 @@
-//will be adding images later
-//will work on scoring/buttons/etc after I get flipping to work
 
-//original card flip
-// const cards = document.querySelectorAll(".card");
-//  for (let i = 0; i < cards.length; i++) {
-//      cards[i].addEventListener("click", function() {
-//        cards[i].classList.toggle("is-flipped");
-//      });
-//  }
+//build - adding my arrays to start game logic
 
-//alt card flip 
-// document.querySelectorAll(".card").forEach(cards => 
-//     cards.addEventListener("click", () => cards.classList.toggle("is-flipped"))
-// );
-//this one ^^^ came from stackoverflow where I was trying to get this to work... what I'm not understanding is why it doesn't toggle back when I click it a second time
+const cardArray = [];
+//the below arrays are to hold cards as they are chosen/matched
+let chosenCards = [];
+let chosenCardsId = [];
+//for scoring
+const cardsMatched = [];
 
-//third attempt at a card flip that toggles on and off
-// const flipCard = function() {
-//     this.classList.toggle(`is-flipped`);
-// }
-// let card = document.getElementsByClassName(`.card`);
-// let cards = [...card];
-// for (let i = 0; i < cards.length; i++) {
-//     cards[i].addEventListener(`click`, flipCard());
-// };
-
-//fourth attempt
+//const for card flip
 const cards = document.querySelectorAll('.card');
-
+//working flip card function
+//took several iterations to find a flip card that worked as desired
 function flipCard() {
   this.classList.toggle('flip');
 }
-
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//slightly stuck till I get my images - will try to find some placehold images easily swapped out
+
+//shuffle logic for game start - fisher/yates algorithm
+shuffleCards(cardArray) {
+    for (let i = cardArray.length -1; i > 0; i--) {
+        let randIndex = Math.floor(Math.random() * (i = 1));
+        cardArray[randIndex].style.order = i;
+        cardArray[i].style.order = randIndex;
+    }
+};
 
 //scoring
     //matches will keep track of the number of matches made till all are made
     //flips keep track of how many times you have flipped cards to try and find a match
     //a win = all cards matched
-    //---->maybe add a timer so there's a way to lose.
+    //---->maybe add a timer so there's a way to lose if I have time
