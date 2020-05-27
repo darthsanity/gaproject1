f8e68309504a6c85c5761e1d2c997c31513e09a9
Tracy Yeast-Rowser <tyeast-rowser@humana.com>
2020-05-27 19:04:51 -0400

    backup images added as a just in case and to work on functionality

--- flashcard.js
+++ flashcard.js
@@ -20,16 +20,18 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 //slightly stuck till I get my images - will try to find some placehold images easily swapped out
 
 //shuffle logic for game start - fisher/yates algorithm
-shuffleCards(cardArray) {
-    for (let i = cardArray.length -1; i > 0; i--) {
-        let randIndex = Math.floor(Math.random() * (i = 1));
-        cardArray[randIndex].style.order = i;
-        cardArray[i].style.order = randIndex;
-    }
-};
+// shuffleCards(cardArray) {
+//     for (let i = cardArray.length -1; i > 0; i--) {
+//         let randIndex = Math.floor(Math.random() * (i = 1));
+//         cardArray[randIndex].style.order = i;
+//         cardArray[i].style.order = randIndex;
+//     }
+// };
 
 //scoring
     //matches will keep track of the number of matches made till all are made
     //flips keep track of how many times you have flipped cards to try and find a match
     //a win = all cards matched
     //---->maybe add a timer so there's a way to lose if I have time
+
+//replay button functionality
